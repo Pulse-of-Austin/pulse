c45f245dd82fb310bcc4b2d451fb277f56512c8e
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 15:17:49 -0500

    Initialize splash page

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
 import './style.scss';
 
-import Main from './components/Main.jsx';
+import SplashPage from './components/SplashPage.jsx';
 import Profile from './components/Profile.jsx';
-import Favorites from './components/Favorites.jsx';
+import Learn from './components/Learn.jsx';
+import WeighIn from './components/WeighIn.jsx';
+import Know from './components/Know.jsx';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 const App = () => (
-  <Main />
+  <SplashPage />
 );
 
 
@@ -28,7 +30,9 @@ ReactDOM.render(
       <div>
         <Route path='/' component={App} />
         <Route path='/profile' component={Profile} />
-        <Route path='/favorites' component={Favorites} />
+        <Route path='/learn' component={Learn} />
+        <Route path='/weign-in' component={WeighIn} />
+        <Route path='/know' component={Know} />
       </div>
   </BrowserRouter>
   </Provider>
