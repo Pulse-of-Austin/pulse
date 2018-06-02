b6a9fb81d25d66b54cd21942cafd75f6059f389c
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 15:50:40 -0500

    Allow for refresh fallback

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
