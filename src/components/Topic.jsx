cc974977d098e0e3ba7f1e842e95be4d22b24ca9
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 18:11:18 -0500

    Render category list and topic list

---
+++ src/components/Topic.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Topic extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div />
+		)
+	}
+}
+
+export default Topic;
