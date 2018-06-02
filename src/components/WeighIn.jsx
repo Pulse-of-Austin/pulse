5c117e015adc54abf18ef6577868007225b1302f
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 16:11:14 -0500

    Add category component

--- src/components/WeighIn.jsx
+++ src/components/WeighIn.jsx
@@ -3,16 +3,17 @@ import { Grid, Accordion } from 'semantic-ui-react';
 
 import NavBar from './NavBar.jsx';
 import Footer from './Footer.jsx';
+import Category from './Category.jsx';
 
 class WeighIn extends React.Component {
 	constructor(props) {
 		super(props);
-
+		
 		this.state = {
-
+			categories: []
 		}
 	}
-
+	
 	render() {
 		return (
 			<Grid>
@@ -20,6 +21,9 @@ class WeighIn extends React.Component {
 					<NavBar />
 				</Grid.Row>
 				<Grid.Row style={{ marginTop: '100px' }}>
+					<Accordion style={{marginTop: '200px'}}>
+						{this.state.categories.map(category => <Category category={category}/>)}
+					</Accordion>
 				</Grid.Row>
 				<Grid.Row>
 					<Footer />
