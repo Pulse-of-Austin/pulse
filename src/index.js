e393b2a282cc7d9b965f14cd471bc77e786aad99
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 11:31:03 -0500

    Add navbar and footer

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
 import './style.scss';
 
+import Main from './components/Main.jsx';
+import Login from './components/Login.jsx';
+import Profile from './components/Profile.jsx';
+import Favorites from './components/Favorites.jsx';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 const App = () => (
-  <div>Hello world!</div>
+  <Main />
 );
 
 
@@ -19,6 +24,9 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Route path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/favorites' component={Favorites} />
       </div>
   </BrowserRouter>
   </Provider>
