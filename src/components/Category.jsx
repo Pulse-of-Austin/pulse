4bf6fa3e80cadbf8cd965e4a969e84de69bc44ab
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 16:11:14 -0500

    Add category component

---
+++ src/components/Category.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Accordion } from 'semantic-ui-react';
+
+class Category extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<Accordion.Title>
+				</Accordion.Title>
+				<Accordion.Content>
+				</Accordion.Content>
+			</div>
+		)
+	}
+}
+
+export default Category;
