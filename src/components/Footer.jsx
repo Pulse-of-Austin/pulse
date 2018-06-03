611a3e58b12fbd7fe5f1ee84943450503b82323c
haileybee1231 <haileybee1231@gmail.com>
2018-06-02 22:47:00 -0500

    Footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Grid, Menu, Image, Divider, Icon, Header } from 'semantic-ui-react';
 
 class Footer extends React.Component {
 	constructor(props) {
@@ -12,31 +12,19 @@ class Footer extends React.Component {
 
 	render() {
 		return (
-			<Menu
-				size='mini'
-				fixed='bottom'
-				inverted={true}
-				style={{ minHeight: '40px' }}
-			>
-				<Menu.Menu position='left'>
-					<Menu.Item position='left'>
-						&copy; 2018 &mdash; Pulse of Austin
-					</Menu.Item>
-					<div className="ui three column very relaxed grid">
-  					<div className="column">
-    					<p>About Us</p>
-    					<p>Contact</p>
-    					<p>Terms and Conditions</p>
-  					</div>
-						<div className="ui vertical divider"> and </div>
- 							<div className="column">
-	 							<p>Facebook</p>
-								<p>Twitter</p>
-							  <p>Instagram</p>
-							 </div>
-						</div>
-							 
-				</Menu.Menu>
+			<Menu inverted={true} fixed='bottom' textAlign='center'>
+				<Grid>
+					<Grid.Row>
+						<Image style={{marginLeft: '45%', marginTop: '10px', marginBottom: '-10px'}} src='./images/pulsefooter.png'/>
+					</Grid.Row>
+					<Grid.Row>
+						<Header style={{marginLeft: '50%'}} as='h3' content='ABOUT US  |  CONTACT US  |  FAQS' />
+					</Grid.Row>
+					<Grid.Row>
+						<Icon name='instagram' size='big' link={true} inverted={true} />
+						<Icon name='facebook' size='big' link={true} inverted={true}/>
+					</Grid.Row>
+				</Grid>
 			</Menu>
 		)
 	}
--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -64,6 +64,8 @@ class SplashPage extends React.Component {
 					</div>
 				</section>
 
+				<Footer />
+
 			</div>
 
 			// <Grid>
